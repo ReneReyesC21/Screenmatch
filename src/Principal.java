824c514ef9dac3950aaabfea4828ddf2272a5be7
Rene <canedarene@gmail.com>
2024-07-09 20:41:24 -0600

    cambiando a setter y getter con el fin de poder reducir mas codigo asi como hacer que el usuario no pueda modificar a su gusto las calificaciones o valores dados por otros usuarios

--- src/Principal.java
+++ src/Principal.java
@@ -1,23 +1,36 @@
+import com.reyeda.screenmatch.modelos.Pelicula;
+
 public class Principal {
+    //private es una restriccion que se puede usar para que otro usuario no pueda modificar un dato
 
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Avengers";
-        miPelicula.fechaDeLanzamiento = 2010;
-        miPelicula.duracionEnMinutos = 90;
+        miPelicula.setNombre("Avengers");
+        miPelicula.setFechaDeLanzamiento(2010);
+        miPelicula.setDuracionEnMinutos(90);
+        miPelicula.setInluidoPlan(true);
 //llamada de metodo void
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(8);
         miPelicula.evalua(10);
-
-        System.out.println(miPelicula.sumaEval);
-        System.out.println(miPelicula.totalEval);
+        miPelicula.evalua(7.5);
+//print
+        System.out.println(miPelicula.getTotalEval());
         System.out.println(miPelicula.calculaMedia());
 
-        Pelicula otraPeli = new Pelicula();
-        otraPeli.nombre = "Batman vs Superman";
-        otraPeli.fechaDeLanzamiento = 2024;
-        otraPeli.duracionEnMinutos = 90;
+
+
+
+
+
+
+
+
+        //otro valor
+//        com.reyeda.screenmatch.modelos.Pelicula otraPeli = new com.reyeda.screenmatch.modelos.Pelicula();
+//        otraPeli.nombre = "Batman vs Superman";
+//        otraPeli.fechaDeLanzamiento = 2024;
+//        otraPeli.duracionEnMinutos = 90;
 //llamada de metodo void
 //        otraPeli.muestraFichaTecnica();
 //        otraPeli.evalua(7);
